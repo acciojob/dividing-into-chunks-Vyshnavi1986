e755f6dd90b74b68005581dfefc2b78481067b6e
Vyshnavi1986 <157476467+Vyshnavi1986@users.noreply.github.com>
2024-04-22 11:30:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,29 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	// Pick a starting point
+    for (let i = 0; i < n; i++) {
+        let currentSum = arr[i];
+ 
+        if (currentSum == sum) {
+            console.log("Sum found at indexes " +i);
+            return;
+        }
+        else {
+            // Try all subarrays starting with 'i'
+            for (let j = i + 1; j < n; j++) {
+                currentSum += arr[j];
+ 
+                if (currentSum == sum) {
+                    console.log("Sum found between indexes "
+                         + i + " and " +j);
+                    return;
+                }
+            }
+        }
+    }
+    console.log("No subarray found");
+    return;
 };
 
 const n = prompt("Enter n: ");
